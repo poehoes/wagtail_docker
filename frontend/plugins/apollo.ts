32ad91970327cf504bed1a1f099f94bbb0dc0f07
Robbie <poehoes@xs4all.nl>
2023-09-01 23:29:52 +0200

    Delete frontend directory

--- frontend/plugins/apollo.ts
+++
@@ -1,9 +0,0 @@
- export default defineNuxtPlugin((nuxtApp) => {
-//     const { wagtailToken } = useRuntimeConfig();
-//     nuxtApp.hook("apollo:auth", ({ client, token }) => {
-//       // `client` can be used to differentiate logic on a per client basis.
-//       // apply apollo client token
-//       token.value = wailtagToken;
-//     });
-
- });
